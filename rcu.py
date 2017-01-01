74a50cf88d794b4358a47d43839ee5a33c0667a2
Suresh Sundriyal <sureshsundriyal@gmail.com>
2017-01-01 00:31:02 -0800

    Rename internal data structures.

--- rcu.py
+++ rcu.py
@@ -16,7 +16,7 @@ class RCUCache(object): # pylint: disable=too-few-public-methods
     '''
 
     # The following makes the object unextendable.
-    __slots__ = ('maxsize', 'items', 'modification_guard', '_setattr')
+    __slots__ = ('maxsize', 'odict', 'modification_guard', '_setattr')
 
     # The following values cannot be pickled.
     _unpicklable = set(['modification_guard', '_setattr'])
@@ -35,7 +35,7 @@ class RCUCache(object): # pylint: disable=too-few-public-methods
                                 super(self.__class__, self).__setattr__)
         # pylint: disable=no-member
         self._setattr('maxsize', maxsize)
-        self._setattr('items', OrderedDict(items))
+        self._setattr('odict', OrderedDict(items))
         self._setattr('modification_guard', Lock())
         # pylint: enable=no-member
 
@@ -64,34 +64,34 @@ class RCUCache(object): # pylint: disable=too-few-public-methods
                         % self.__class__.__name__)
 
     def __getitem__(self, key):
-        return self.items[key] # pylint: disable=no-member
+        return self.odict[key] # pylint: disable=no-member
 
     def __setitem__(self, key, value):
         # pylint: disable=no-member
         with self.modification_guard:
-            temp_items = OrderedDict(self.items)
-            if self.maxsize and len(self.items) == self.maxsize:
-                temp_items.popitem(last=False)
-            temp_items[key] = value
-            self._setattr('items', temp_items)
+            temp_odict = OrderedDict(self.odict)
+            if self.maxsize and len(self.odict) == self.maxsize:
+                temp_odict.popitem(last=False)
+            temp_odict[key] = value
+            self._setattr('odict', temp_odict)
         # pylint: enable=no-member
 
     def __delitem__(self, key):
         # pylint: disable=no-member
         with self.modification_guard:
-            temp_items = OrderedDict(self.items)
-            del temp_items[key]
-            self._setattr('items', temp_items)
+            temp_odict = OrderedDict(self.odict)
+            del temp_odict[key]
+            self._setattr('odict', temp_odict)
         # pylint: enable=no-member
 
     def __repr__(self):
         # pylint: disable=no-member
         return '%s(maxsize=%s, items=%r)' % (self.__class__.__name__,
-                                          self.maxsize, self.items)
+                                          self.maxsize, self.odict)
         # pylint: enable=no-member
 
     def __len__(self):
-        return len(self.items) # pylint: disable=no-member
+        return len(self.odict) # pylint: disable=no-member
 
     def clear(self):
         '''
@@ -99,48 +99,48 @@ class RCUCache(object): # pylint: disable=too-few-public-methods
         '''
         # pylint: disable=no-member
         with self.modification_guard:
-            self._setattr('items', OrderedDict())
+            self._setattr('odict', OrderedDict())
         # pylint: enable=no-member
 
     def copy(self):
         '''
         Create a copy of the object
         '''
-        return self.__class__(self.maxsize, self.items) # pylint: disable=no-member
+        return self.__class__(self.maxsize, self.odict) # pylint: disable=no-member
 
     def update(self, *args, **kwargs):
         # pylint: disable=no-member
         with self.modification_guard:
-            temp_items = OrderedDict(self.items)
-            temp_items.update(*args, **kwargs)
-            if self.maxsize and len(temp_items) > self.maxsize:
-                for _ in repeat(None, (len(temp_items) - self.maxsize)):
-                    temp_items.popitem(last=False)
-            self._setattr('items', temp_items)
+            temp_odict = OrderedDict(self.odict)
+            temp_odict.update(*args, **kwargs)
+            if self.maxsize and len(temp_odict) > self.maxsize:
+                for _ in repeat(None, (len(temp_odict) - self.maxsize)):
+                    temp_odict.popitem(last=False)
+            self._setattr('odict', temp_odict)
         # pylint: enable=no-member
 
     def popitem(self, last=True):
         # pylint: disable=no-member
         with self.modification_guard:
-            temp_items = OrderedDict(self.items)
-            retval = temp_items.popitem(last=last)
-            self._setattr('items', temp_items)
+            temp_odict = OrderedDict(self.odict)
+            retval = temp_odict.popitem(last=last)
+            self._setattr('odict', temp_odict)
             return retval
         # pylint: enable=no-member
 
     def pop(self, *args, **kwargs):
         # pylint: disable=no-member
         with self.modification_guard:
-            temp_items = OrderedDict(self.items)
-            retval = temp_items.pop(*args, **kwargs)
-            self._setattr('items', temp_items)
+            temp_odict = OrderedDict(self.odict)
+            retval = temp_odict.pop(*args, **kwargs)
+            self._setattr('odict', temp_odict)
             return retval
         # pylint: enable=no-member
 
     def items(self, *args, **kwargs):
-        tempRef = self.items
-        return items(tempRef)
+        tempRef = self.odict
+        return tempRef.items()
 
     def __iter__(self, *args, **kwargs):
-        tempRef = self.items
+        tempRef = self.odict
         return iter(tempRef)
